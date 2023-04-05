722df01c478badcec554ac9d6fd6c05c912f05c7
Michael Mount <mount.photos@gmail.com>
2023-04-04 21:00:28 -0600

    working on seeding data

---
+++ utils/data.js
@@ -0,0 +1,44 @@
+const userNames = [
+  "mmount",
+  "edaClaw",
+  "AzuraDaWitch",
+  "AmityB",
+  "LuzNoceda",
+  "KingClaw",
+];
+
+const emails = [
+  "mmount@gmail.com",
+  "badGirlCoven@gmail.com",
+  "AzuraBookClub@gmaail.com",
+  "devWorld@gmail.com",
+  "apple@gmail.com",
+  "covenMate@gmail.com",
+];
+
+const thoughts = [
+  "We should fight people",
+  "Coding Is cool",
+  "Grass is not as good as succulents",
+  "Apple need usb-c charging ages ago",
+  "Studio Ghibli is a great production company",
+  "I would love to go to Japan some day",
+  "The human world is strange",
+  "German is cool",
+  "I am tired",
+];
+
+const reactions = [
+  "You are so right!",
+  "No I disgree with this",
+  "Im glad I have people who share the same thoughts",
+  "Im tired",
+  "No that is just what the goverment wants you to think",
+  "The Owl House is a great show",
+  "I miss you!",
+  "Apple is overrated",
+  "I love you!",
+  "You are my hero for saying this!",
+];
+
+module.exports = { userNames, emails, thoughts, reactions };
