7be2f1936f5dad33b032678b6e805adb389d7323
Michael Mount <mount.photos@gmail.com>
2023-04-04 22:26:34 -0600

    built route for removing friends from user friend list

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,13 +1,13 @@
 const User = require("../models/User");
 
 module.exports = {
-  //responds with all Users
+  //Responds with all Users
   getUsers(req, res) {
     User.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-  //responds with one user by id
+  //Responds with one user by id
   getUserId(req, res) {
     User.findOne({ _id: req.params.userId })
       .then((user) =>
@@ -17,11 +17,13 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  //creates User
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+  //Updates User Info by Id
   updateUser(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -38,6 +40,7 @@ module.exports = {
         res.status(500).json(err);
       });
   },
+  //Delete User by Id
   deleteUser(req, res) {
     //deletes the User
     User.findOneAndRemove({ _id: req.params.userId })
@@ -51,5 +54,33 @@ module.exports = {
         console.log(err);
         res.status(500).json(err);
       });
+  },
+  //Adds Friend to Users List
+  addFriendToUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User Found" })
+          : res.json(user)
+      )
+      .catch((err = res.status(500).json(err)));
+  },
+  //Deletes Friend from Users List
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User Found" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
 };
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -5,11 +5,18 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriendToUser,
+  removeFriend,
 } = require("../../controllers/usersController");
 
 //api/users
 router.route("/").get(getUsers).post(createUser);
 
 router.route("/:userId").get(getUserId).put(updateUser).delete(deleteUser);
+
+router
+  .route("/:userId/friends/:friendId")
+  .post(addFriendToUser)
+  .delete(removeFriend);
 
 module.exports = router;
