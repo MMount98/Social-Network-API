f5869221b20b7c08a9787c601b0378292b464db9
Michael Mount <mount.photos@gmail.com>
2023-04-06 21:56:10 -0600

    fixed user id bug

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,7 +14,7 @@ module.exports = {
       .then((user) =>
         !user
           ? res.status(404).json({ message: "No user found" })
-          : User.deleteMany({ thoughts: req.params.userId })
+          : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
